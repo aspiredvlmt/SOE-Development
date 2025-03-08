56edfb22a13b5ebe95e5e06c758f41580249abca
zandro Joaquin Cruz <147904451+ZandroCruz27@users.noreply.github.com>
2025-03-08 16:29:35 +0800

    Add files via upload

--- formselection.jsx
+++ formselection.jsx
@@ -6,47 +6,7 @@ import "./formselection.css";
 const FormSelectionUI = ({ onSelectForm }) => {
   return (
     <div className="form-selection-container">
-      <div className="sidebar">
-        <h4 className="sidebar-title">Kronos</h4>
-        <ul className="nav-list">
-        <li className="nav-item mb-3">
-            <a href="#" className="nav-link active text-white bg-primary d-flex align-items-center">
-                <i className="bi bi-file-earmark-text me-2"></i> Reservation Forms
-            </a>
-            </li>
-            <li className="nav-item mb-3">
-            <a href="#" className="nav-link text-dark d-flex align-items-center">
-                <i className="bi bi-folder me-2"></i> Records
-            </a>
-            </li>
-            <li className="nav-item mb-3">
-            <a href="#" className="nav-link text-dark d-flex align-items-center">
-                <i className="bi bi-card-checklist me-2"></i> Request Management
-            </a>
-            </li>
-            <li className="nav-item mb-3">
-            <a href="#" className="nav-link text-dark d-flex align-items-center">
-                <i className="bi bi-bar-chart me-2"></i> Reports
-            </a>
-            </li>
-            <li className="nav-item mb-3">
-            <a href="#" className="nav-link text-dark d-flex align-items-center">
-                <i className="bi bi-building me-2"></i> Properties
-            </a>
-            </li>
-            <li className="nav-item mb-3">
-            <a href="#" className="nav-link text-dark d-flex align-items-center">
-                <i className="bi bi-people me-2"></i> User Management
-            </a>
-            </li>
-            <li className="nav-item mb-3">
-            <a href="#" className="nav-link text-danger d-flex align-items-center">
-                <i className="bi bi-box-arrow-right me-2"></i> Logout
-            </a>
-        </li>
-        </ul>
-        
-    </div>
+      
 
       
       <div className="main-content">
