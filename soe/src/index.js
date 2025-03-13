f59be0c3dfb37a39f82d07302f7db3c188f82429
Daniel Fajardo <151352808+fajardoDaniel@users.noreply.github.com>
2025-03-13 19:06:17 +0800

    Update_Req Management

--- soe/src/index.js
+++ soe/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from '../src/PurchaseRequisitionForm';
+import App from './RequestManagement';
 import reportWebVitals from './reportWebVitals';
 
 
