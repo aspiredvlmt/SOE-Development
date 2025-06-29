7a135c56085ec3895660637db721f2188201330b
zandro Joaquin Cruz <147904451+ZandroCruz27@users.noreply.github.com>
2025-06-29 17:10:20 +0800

    Add files via upload

---
+++ kronos-signup/App.js
@@ -0,0 +1,12 @@
+import KronosSignup from './KronosSignup';
+import './KronosSignup.css';
+
+function App() {
+  return (
+    <div className="App">
+      <KronosSignup />
+    </div>
+  );
+}
+
+export default App;
